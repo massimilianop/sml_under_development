c8084604c36c09c7bc8bcd55f8a4846882a0d28b
Antonio <antonio.adaldo.89@gmail.com>
2016-03-25 20:48:49 +0100

    Added hierarchy and json construction for yaw controllers. Tested from command-line, not from GUI

--- quad_control/setup.py
+++ quad_control/setup.py
@@ -5,7 +5,10 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 # fetch values from package.xml
 setup_args = generate_distutils_setup(
-    packages=['utilities', 'simulators_hierarchical'],
+    packages=[
+        'utilities',
+        'simulators_hierarchical',
+        'yaw_controllers'],
     package_dir={'': 'scripts'},
 )
 
