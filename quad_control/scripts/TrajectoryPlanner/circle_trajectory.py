Fixed serialization of offset and rotation in `Trajectory` objects. Now the numpy arrays are converted to lists before json serialization. Viceversa, when json loads offset and rotation from a string, they are also converted to numpy arrays. The `Trajectory` class takes care of this conversion. No need to do it in the GUI or in the child trajectories. This is needed

quad_control/scripts/TrajectoryPlanner/circle_trajectory.py
@@ -63,8 +63,11 @@ class CircleTrajectory(tj.Trajectory):
 #tr = CircleTrajectory(offset=np.array([0.0, 0.0, 1.0]), radius=0.5, speed=0.2)
 ##print tr
 
-#string = tr.parameters_to_string(0.5, 0.2)
+#string = CircleTrajectory.offset_and_rotation_to_string()
 #print string
-#params = tr.string_to_parameters(string)
+#offset, rotation = CircleTrajectory.string_to_offset_and_rotation(string)
+#print offset, rotation
+#traj = CircleTrajectory(offset, rotation)
+#print traj
 
-#print params
+