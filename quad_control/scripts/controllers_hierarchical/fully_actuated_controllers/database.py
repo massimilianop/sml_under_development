e3f194487b48cbda95d0be07edb3fc8910974f80
Antonio <antonio.adaldo.89@gmail.com>
2016-03-29 15:47:13 +0200

    Added a missing line in the fully actuated controllers database

--- quad_control/scripts/controllers_hierarchical/fully_actuated_controllers/database.py
+++ quad_control/scripts/controllers_hierarchical/fully_actuated_controllers/database.py
@@ -1,8 +1,10 @@
 #TODO add an abstract fully-actuated controller to import
 import neutral_controller as nc
 import simple_bounded_integral_pid_controller as sbipidc
+import bounded_integral_pid_controller as bipidc
 
 data = {
 "NeutralController": nc.NeutralController,
 "SimpleBoundedIntegralPIDController": sbipidc.SimpleBoundedIntegralPIDController
+"BoundedIntegralPIDController": bipidc.BoundedIntegralPIDController
 }
