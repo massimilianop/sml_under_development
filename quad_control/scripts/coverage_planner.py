b04ac10333ee83f4025f97d8b19632cb3fbefb9c
Antonio <antonio.adaldo.89@gmail.com>
2016-05-08 23:10:03 +0200

    A simplified version of the coverage algorithm.
    Launch `coverage.launch` for a demo.
    Swap the `planar_integrator.py` simulator with a system of your choice.
    A velocity-control interface is necessary, since the coverage plan gives velocity commands.
    (A velocity-control interface is available in RotorS.)

--- quad_control/scripts/coverage_planner.py
+++ quad_control/scripts/coverage_planner.py
@@ -16,24 +16,6 @@ import random as rdm
 
 
 
-def pose2D_from_pos_ver(pos, ver):
-    angle = angle_from_versor(ver)
-    return gms.Pose2D(pos[0], pos[1], angle)
-    
-def pos_ver_from_pose2D(pose):
-    pos = np.array([pose.x, pose.y])
-    ver = versor_from_angle(pose.theta)
-    return pos, ver
-    
-    
-def landmark_from_pos(pos):
-    return qms.Landmark(pos[0], pos[1])
-    
-def pos_from_landmark(lmk):
-    return np.array([lmk.x, lmk.y])
-
-
-
 
 
 __NAME = rp.get_param('name', 'Axel')
@@ -47,9 +29,7 @@ __landmarks = cov.INITIAL_LANDMARKS_LISTS[__NAME]
 __position = np.zeros(2)
 __versor = cov.versor_from_angle(0.0)
 __possible_trade_partners = list(__OTHERS_NAMES)
-
-
-
+__token = rp.get_param('token', False)
 
 
 
@@ -59,44 +39,67 @@ __possible_trade_partners = list(__OTHERS_NAMES)
 def __trade_landmarks_handler(request):
     global __pose_lock, __trade_lock
     global __position, __versor, __landmarks
-    __trade_lock.acquire()
+    global __NAME, __OTHERS_NAMES, __possible_trade_partners
     __pose_lock.acquire()
     p1 = __position
     v1 = __versor
-    p2 = np.array([request.pose.x, request.pose.y])
-    v2 = cov.versor_from_angle(request.pose.theta)
+    __pose_lock.release()
+    __trade_lock.acquire()
     lst1 = __landmarks
     lst2 = [cov.pos_from_landmark(lmk) for lmk in request.landmarks]
-    result = reassign_landmarks(__pose, request.pose, __landmarks, request.landmarks)
-    success, remove_from_1, add_to_1, remove_from_2, add_to_2 = result
+    p2, v2 = cov.pos_ver_from_pose2D(request.pose)
+    success, remove_from_1, add_to_1, remove_from_2, add_to_2 = cov.reassign_landmarks(p1, v1, p2, v2, lst1, lst2)
     if success:
-        __landmarks = [__landmarks[index] for index in range(len(__landmarks))-remove_from_1]
+        __landmarks = [__landmarks[index] for index in filter(lambda z: not z in remove_from_1, range(len(__landmarks)))]
         __landmarks += add_to_1
         __possible_trade_partners = list(__OTHERS_NAMES)
     else:
         if request.name in __possible_trade_partners:
             __possible_trade_partners.remove(request.name)
-    __pose_lock.release()
     __trade_lock.release()
+    add_to_2 = [cov.landmark_from_pos(pos) for pos in add_to_2]
+    rp.logwarn(request.name + ' requests a trade with ' + __NAME)
+    rp.logwarn('Success of the trade: ' + str(success))
     return success, remove_from_2, add_to_2
+
+
+def __receive_token_handler(request):
+    global __trade_lock
+    global __possible_trade_partners
+    global __token
+    __trade_lock.acquire()
+    if len(__possible_trade_partners) > 0:
+        __token = True
+        result = True
+        rp.logwarn(__NAME + ': token accepted')
+    else:
+        result = False
+        rp.logwarn(__NAME + ': token refused')
+    __trade_lock.release()
+    return result
+    
     
     
 def __pose_callback(pose):
     global __pose_lock
     global __position, __versor
     __pose_lock.acquire()
-    __position = np.array([pose.x, pose.y])
-    __versor = cov.versor_from_angle(pose.theta)
+    __position, __versor = cov.pos_ver_from_pose2D(pose)
     __pose_lock.release()
     
     
     
 rp.init_node('coverage_planner')
+__initial_time = rp.get_time()
 __RATE = rp.Rate(1e2)
 rp.Subscriber('pose_2d', gms.Pose2D, __pose_callback)
-rp.Service('trade_service', qsv.TradeLandmarks, __trade_landmarks_handler)
-proxies = [rp.ServiceProxy(name + '/trade_service', qsv.TradeLandmarks)
-    for name in __OTHERS_NAMES]
+rp.Service('trade_landmarks', qsv.TradeLandmarks, __trade_landmarks_handler)
+rp.Service('receive_token', qsv.ReceiveToken, __receive_token_handler)
+__trade_proxies = {}
+__token_proxies = {}
+for name in __OTHERS_NAMES:
+    __trade_proxies[name] = rp.ServiceProxy('/' + name + '/trade_landmarks', qsv.TradeLandmarks)
+    __token_proxies[name] = rp.ServiceProxy('/' + name + '/receive_token', qsv.ReceiveToken)
 __cmd_vel_pub = rp.Publisher('cmd_vel', gms.Pose2D, queue_size=10)
 __cov_pub = rp.Publisher('coverage', sms.Float64, queue_size=10)
 
@@ -104,21 +107,54 @@ __cov_pub = rp.Publisher('coverage', sms.Float64, queue_size=10)
 def __work():
     global __pose_lock, __trade_lock
     global __position, __versor, __landmarks
-    global __pub
+    global __NAME, __OTHERS_NAMES, __possible_trade_partners
+    global __token
+    global __cmd_vel_pub, __cov_pub
+    global __initial_time
     __pose_lock.acquire()
     __trade_lock.acquire()
     dot_p = cov.coverage_gradient_pos(__position, __versor, __landmarks)
     dot_theta = cov.coverage_gradient_ver(__position, __versor, __landmarks)
     coverage = cov.coverage_function(__position, __versor, __landmarks)
+    pose = gms.Pose2D(__position[0], __position[1], cov.angle_from_versor(__versor))
     __trade_lock.release()
     __pose_lock.release()
-    #if np.linalg.norm(dot_p) > 0.3:
-    #    dot_p *= 0.3/np.linalg.norm(dot_p)
-    #dot_theta = np.clip(dot_theta, -0.1, 0.1)
+    if np.linalg.norm(dot_p) > 10.3:
+        dot_p *= 10.3/np.linalg.norm(dot_p)
+    dot_theta = np.clip(dot_theta, -10.0, 10.0)
     cmd_vel = gms.Pose2D(dot_p[0], dot_p[1], dot_theta)
-    #msg = gms.Pose2D()
     __cmd_vel_pub.publish(cmd_vel)
     __cov_pub.publish(coverage)
+    __trade_lock.acquire()
+    if np.linalg.norm(dot_p) < __TOLERANCE and np.linalg.norm(dot_theta) < __TOLERANCE and __token:
+        if len(__possible_trade_partners)>0:
+            lmks = [cov.landmark_from_pos(lmk) for lmk in __landmarks]
+            name = rdm.choice(__possible_trade_partners)
+            rsp = __trade_proxies[name](__NAME, pose, lmks)
+            if rsp.success:
+                __possible_trade_partners = list(__OTHERS_NAMES)
+                __landmarks = [__landmarks[index] for index in filter(lambda z: not z in rsp.to_remove, range(len(__landmarks)))]
+                __landmarks += [cov.pos_from_landmark(lmk) for lmk in rsp.to_add]
+            else:
+                __possible_trade_partners.remove(name)
+        token_accepted = False
+        possible_token_receivers = list(__OTHERS_NAMES)
+        while not token_accepted and len(possible_token_receivers)>0:
+            rp.logwarn(__NAME + ': possible token receivers: ' + str(possible_token_receivers))
+            name = rdm.choice(possible_token_receivers)
+            possible_token_receivers.remove(name)
+            rsp = __token_proxies[name]()
+            token_accepted = rsp.accepted
+        if token_accepted:
+            __token = False
+        elif len(__possible_trade_partners) == 0:
+            final_time = rp.get_time()
+            rp.logwarn('Finish! Elapsed time: ' + str(final_time-__initial_time))
+            __token = False
+    __trade_lock.release()
+    
+    
+
 
 
 while not rp.is_shutdown():
--- quad_control/src/utilities/coverage_utilities.py
+++ quad_control/src/utilities/coverage_utilities.py
@@ -5,10 +5,29 @@ gradient-based coverage algorithm.
 import numpy as np
 import random as rdm
 import tf.transformations as tfm
+import geometry_msgs.msg as gms
+import quad_control.msg as qms
 
 
 TOLERANCE = 0.01
 
+
+def pose2D_from_pos_ver(pos, ver):
+    angle = angle_from_versor(ver)
+    return gms.Pose2D(pos[0], pos[1], angle)
+    
+def pos_ver_from_pose2D(pose):
+    pos = np.array([pose.x, pose.y])
+    ver = versor_from_angle(pose.theta)
+    return pos, ver
+    
+    
+def landmark_from_pos(pos):
+    return qms.Landmark(pos[0], pos[1])
+    
+def pos_from_landmark(lmk):
+    return np.array([lmk.x, lmk.y])
+    
 
 
 def versor_gradient(ver):
@@ -71,12 +90,12 @@ def reassign_landmarks(p1, v1, p2, v2, lst1, lst2):
     for i, q in enumerate(lst1):
         if visibility_function(p2, v2, q) > visibility_function(p1, v1, q) + TOLERANCE:
             remove_from_1.append(i)
-            give_to_2.append(q)
+            add_to_2.append(q)
             success = True
     for i, q in enumerate(lst2):
         if visibility_function(p1, v1, q) > visibility_function(p2, v2, q) + TOLERANCE:
             remove_from_2.append(i)
-            give_to_1.append(q)
+            add_to_1.append(q)
             success = True
     return success, remove_from_1, add_to_1, remove_from_2, add_to_2
     
