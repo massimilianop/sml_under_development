8625e899a04a91d086d841752b73744d9a524074
potaopereira <pedro.m.otao.pereira@gmail.com>
2016-05-25 21:08:50 +0200

    Document node that provides time triggered service request

--- quad_control/scripts/service_time_sequencer.py
+++ quad_control/scripts/service_time_sequencer.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-# this line is just used to define the type of document
+"""Class ServiceTimeSequencer that is used in node that accepts a sequence of services with trigger instants and sends these services according to these trigger instants"""
 
 import rospy
 
@@ -7,21 +6,17 @@ import rospy
 from quad_control.srv import *
 
 services_database = {}
-services_database['ServiceChangeMission']       = SrvCreateJsonableObjectByStr
-services_database['ServiceChangeReference']     = SrvCreateJsonableObjectByStr
-services_database['ServiceChangeYawReference']  = SrvCreateJsonableObjectByStr
-services_database['ServiceChangeYawController'] = SrvCreateJsonableObjectByStr
-services_database['ServiceChangeController']    = SrvCreateJsonableObjectByStr
-services_database['ServiceMissionChangeInner']         = SrvChangeJsonableObjectByStr
+services_database['ServiceChangeMission']      = SrvCreateJsonableObjectByStr
+services_database['ServiceMissionChangeInner'] = SrvChangeJsonableObjectByStr
 
 services_database['IrisPlusResetNeutral']    = IrisPlusResetNeutral
 services_database['IrisPlusSetNeutral']      = IrisPlusSetNeutral
- 
 
 import json
 
 # TODO: reply may have other fields
 def request_service(namespace,service_name_str,service_inputs_dic):
+    """Sets up a service based on service name, and dictionary with inputs to service"""
 
     ServiceClass = services_database[service_name_str]
     try: 
@@ -36,19 +31,23 @@ def request_service(namespace,service_name_str,service_inputs_dic):
                 rospy.logwarn('Service '+service_name_str+' provided')
         except rospy.ServiceException as exc:
             rospy.logwarn('Service '+service_name_str+' did not process request: ' + str(exc))
-            pass
+            return
     except rospy.ServiceException as exc:
         rospy.logwarn('Service '+service_name_str+' did not process request: ' + str(exc))
-        pass
+        return
 
 class ServiceTimeSequencer():
 
-    def __init__(self):
-
+    def __init__(self,frequency = 2.0,infinity  = 3600):
+        """Construct node that provides services at frequency not larger than 'frequency' and 'infinity' is taken as an infinite time interval"""
+        
         # frequency of node (Hz)
-        self.frequency = 2.0
+        self.frequency = frequency
 
-    def _handle_service_sequence(self,data):
+        # infinite trigger instant in sec (approx one hour) 
+        self.infinity  = infinity
+
+    def handle_service_sequence(self,data):
         self.service_sequence = json.loads(data.service_sequence)
         self.initial_instant  = rospy.get_time()
 
@@ -56,7 +55,11 @@ class ServiceTimeSequencer():
 
         return ServiceSequenceResponse(received = True)
 
-    def handle_service_sequence(self):
+    def update_and_request(self):
+        """Request for the current service, and update next_trigger_instant, service_name and inputs_to_service"""
+
+        print("asking for: "+self.service_name)
+        request_service(self.namespace,self.service_name,self.inputs_service)
 
         # if service_sequence is non-empty
         if self.service_sequence:
@@ -69,36 +72,35 @@ class ServiceTimeSequencer():
             self.inputs_service       = service['inputs_service']
             
         else:
-            self.next_trigger_instant = 3600
-
-    def update_and_request(self):
-
-        print(self.service_name)
-        request_service(self.namespace,self.service_name,self.inputs_service)
-
-        self.handle_service_sequence()
-
+            # next
+            self.next_trigger_instant = self.infinity
 
     def loop(self):
+        """Sets up service that accepts sequence of services and provides services sequentially"""
 
-        # node will be named quad_control (see rqt_graph)
+        # name node as service_time_sequencer
         rospy.init_node('service_time_sequencer', anonymous=True)
 
         # service for selecting desired trajectory
-        TrajDes_service = rospy.Service('ServiceSequencer', ServiceSequence, self._handle_service_sequence)
+        rospy.Service('ServiceSequencer', ServiceSequence, self.handle_service_sequence)
 
         rate = rospy.Rate(self.frequency)
 
         self.namespace = ''
 
+        # initial_instant is reset every time a nw service_sequence is received
         self.initial_instant      = rospy.get_time()
-        self.next_trigger_instant = 3600
+
+        # next (not absolute) time instant when service is provided
+        self.next_trigger_instant = self.infinity
 
         while not rospy.is_shutdown():
 
-            current_time = rospy.get_time() - self.initial_instant
+            # elapsed time
+            elapsed_time = rospy.get_time() - self.initial_instant
 
-            if current_time >= self.next_trigger_instant:
+            # if elapsed time is bigger than trigger instant
+            if elapsed_time >= self.next_trigger_instant:
                 self.update_and_request()
 
             # go to sleep
