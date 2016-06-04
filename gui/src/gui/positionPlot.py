674cc48958253b3090666a40552c62ea093c4194
potaopereira <pedro.m.otao.pereira@gmail.com>
2016-06-04 15:06:00 +0200

    One pyqtsignal only on the gui positionPlot
    Rather than one pyqtsignal per position component, velocity component, etc,
    just one pqqtsignal is necessary, whose type is now derived from the message
    that the gui is subscribed to
    
    Also, move the labels to the sides of the plots, rather than below the plots

--- gui/src/gui/positionPlot.py
+++ gui/src/gui/positionPlot.py
@@ -12,25 +12,17 @@ import pyqtgraph as pg
 # from python_qt_binding.QtCore import QTimer, Slot
 # from python_qt_binding.QtCore import pyqtSlot
 
-
 import numpy
 
-# import analysis
-# import utils
 import subprocess
-# from mavros.msg import OverrideRCIn
-# from mocap.msg import QuadPositionDerived
 
 from quad_control.msg import quad_state_and_cmd
-
 
 # import services defined in quad_control
 from quad_control.srv import *
 from quad_control.srv import PlotService
 
 import argparse
-
-
 
 import rospkg
 # get an instance of RosPack with the default search paths
@@ -39,57 +31,9 @@ rospack = rospkg.RosPack()
 import sys
 sys.path.insert(0, rospack.get_path('quad_control'))
 
-
 class positionPlotPlugin(Plugin):
 
-    # for time vector
-    Time = pyqtSignal(float)
-
-    # for x,y,z vectors
-    Xon = pyqtSignal(float)
-    Yon = pyqtSignal(float)
-    Zon = pyqtSignal(float)
-    
-    # for desired x,y,z vectors
-    Xdon = pyqtSignal(float)
-    Ydon = pyqtSignal(float)
-    Zdon = pyqtSignal(float)
-
-    # for vx,vy,vz vectors
-    Xvelon = pyqtSignal(float)
-    Yvelon = pyqtSignal(float)
-    Zvelon = pyqtSignal(float)
-
-    # for desired vx,vy,vz vectors
-    Xdvelon = pyqtSignal(float)
-    Ydvelon = pyqtSignal(float)
-    Zdvelon = pyqtSignal(float)
-
-    # for vx,vy,vz vectors
-    Xvelon = pyqtSignal(float)
-    Yvelon = pyqtSignal(float)
-    Zvelon = pyqtSignal(float)
-
-    # for desired vx,vy,vz vectors
-    Xdvelon = pyqtSignal(float)
-    Ydvelon = pyqtSignal(float)
-    Zdvelon = pyqtSignal(float)  
-
-    # for euler angles
-    Roll_on = pyqtSignal(float)
-    Pitch_on = pyqtSignal(float)
-    Yaw_on = pyqtSignal(float)
-    
-    # for desired euler angles
-    Rolld_on = pyqtSignal(float)
-    Pitchd_on = pyqtSignal(float)
-    Yawd_on = pyqtSignal(float)     
-
-    # for channels vectors
-    Ch1on = pyqtSignal(float)
-    Ch2on = pyqtSignal(float)
-    Ch3on = pyqtSignal(float)    
-    Ch4on = pyqtSignal(float)      
+    combined_data = pyqtSignal(quad_state_and_cmd)
 
     # ---------------------------------------------- #
     # ---------------------------------------------- #
@@ -105,8 +49,6 @@ class positionPlotPlugin(Plugin):
     Period_Plot = 1.0
     # Need to know frequency of messages we are subscribing to 
     Frequency_Subscription = 10
-
-
 
 
     def __init__(self, context,namespace = None):
@@ -152,7 +94,6 @@ class positionPlotPlugin(Plugin):
         context.add_widget(self._widget)
 
         # ---------------------------------------------- #
-        # ---------------------------------------------- #
 
         # window for positions
         plotwidget = pg.PlotWidget()
@@ -177,8 +118,6 @@ class positionPlotPlugin(Plugin):
 
 
         # ---------------------------------------------- #
-        # ---------------------------------------------- #        
-        
         layout        = QtGui.QGridLayout()
         vellayout     = QtGui.QGridLayout()
         Angleslayout  = QtGui.QGridLayout()
@@ -191,8 +130,6 @@ class positionPlotPlugin(Plugin):
         channellayout.addWidget(channelplotwidget)
 
         # ---------------------------------------------- #
-        # ---------------------------------------------- #  
-
         # labels for window with positions
         plotwidget.getPlotItem().setLabel('left','position','m')
         plotwidget.getPlotItem().setLabel('bottom','time','s')
@@ -221,7 +158,8 @@ class positionPlotPlugin(Plugin):
         # ---------------------------------------------- #          
 
         # time vector
-        self.timevector = [0]*self.Size_Vector
+        # self.timevector = [0]*self.Size_Vector
+        self.timevector = [i*1.0/self.Frequency_Subscription for i in range(self.Size_Vector)]
 
         
         #Setting variables for each coordinate and channel
@@ -335,83 +273,31 @@ class positionPlotPlugin(Plugin):
 
 
         # ---------------------------------------------- #
-        # ---------------------------------------------- #   
-
-        
         # THIS IS A NECESSARY MEASURE BECAUSE THE UPDATING OF DATA AND
         # THE SUBSCRIPTION RUN ON DIFFERENT THREADS
-
-        # Connecting slots to signals
-        self.Time.connect(self.TimeUpdate)
-
-        # positions x,y,z
-        self.Xon.connect(self.XonUpdate)
-        self.Yon.connect(self.YonUpdate)
-        self.Zon.connect(self.ZonUpdate)
-
-        # desired positions x,y,z
-        self.Xdon.connect(self.XdonUpdate)
-        self.Ydon.connect(self.YdonUpdate)
-        self.Zdon.connect(self.ZdonUpdate)
-
-        # velocities x,y,z
-        self.Xvelon.connect(self.XvelonUpdate)
-        self.Yvelon.connect(self.YvelonUpdate)
-        self.Zvelon.connect(self.ZvelonUpdate)
-
-        # desired velocities x,y,z
-        self.Xdvelon.connect(self.XdvelonUpdate)
-        self.Ydvelon.connect(self.YdvelonUpdate)
-        self.Zdvelon.connect(self.ZdvelonUpdate)
-
-        # Euler angles
-        self.Roll_on.connect(self.Roll_onUpdate)
-        self.Pitch_on.connect(self.Pitch_onUpdate)
-        self.Yaw_on.connect(self.Yaw_onUpdate)
-
-        # Desired Euler angles
-        self.Rolld_on.connect(self.Rolld_onUpdate)
-        self.Pitchd_on.connect(self.Pitchd_onUpdate)
-        self.Yawd_on.connect(self.Yawd_onUpdate)        
-
-        # channels
-        self.Ch1on.connect(self.Ch1onUpdate)
-        self.Ch2on.connect(self.Ch2onUpdate)
-        self.Ch3on.connect(self.Ch3onUpdate) 
-        self.Ch4on.connect(self.Ch4onUpdate)          
+        self.combined_data.connect(self.update_data)
 
         
-        # ---------------------------------------------- #
         # ---------------------------------------------- #
         # counter for data saving
         self.counter = 1
         self.counterBound = numpy.int(self.Period_Data_Saving*self.Frequency_Subscription)
 
-
-        # ---------------------------------------------- #
         # ---------------------------------------------- #
         # counter for plotting
         self.counterPlot = 1
         self.counterBoundPlot = numpy.int(self.Period_Plot*self.Frequency_Subscription)
 
-
-        # ---------------------------------------------- #
         # ---------------------------------------------- #
         # initial time: this will be used to offset to time to 0 
         # instead of plotting with "real" time
         self.time0 = rospy.get_time()
 
-
         # ---------------------------------------------- #
-        # ---------------------------------------------- #
-
         # BUTTON TO SUBSCRIBE AND UNSUBSCRIBE
         self._widget.ButtonSubscribe.stateChanged.connect(self.SetSubscription)
        
-
         # ---------------------------------------------- #
-        # ---------------------------------------------- #
-
         # BUTTON TO ASK FOR SAVING AND STOPPING SAVING AS WELL
         self._widget.ButtonRequestSave.stateChanged.connect(self.SaveDataClient)
 
@@ -483,25 +369,53 @@ class positionPlotPlugin(Plugin):
 
         pass 
 
-    def callback(self,data):
+    def update_data(self,data):
+
+        if self.counter == 1:
+            self.timevector[:-1] = self.timevector[1:]
+            self.timevector[-1]  = data.time - self.time0
 
         
-        # time vector new data        
-        self.Time.emit(data.time)
+        self.VariableOnUpdate(self.Xplotvector,data.x,self.Xcurve,self._widget.Xcheck)
+        self.VariableOnUpdate(self.Yplotvector,data.y,self.Ycurve,self._widget.Ycheck)
+        self.VariableOnUpdate(self.Zplotvector,data.z,self.Zcurve,self._widget.Zcheck)
+        
+        self.VariableOnUpdate(self.Xdplotvector,data.xd,self.Xdcurve,self._widget.Xdcheck)
+        self.VariableOnUpdate(self.Ydplotvector,data.yd,self.Ydcurve,self._widget.Ydcheck)
+        self.VariableOnUpdate(self.Zdplotvector,data.zd,self.Zdcurve,self._widget.Zdcheck)
+    
+        self.VariableOnUpdate(self.Xvelplotvector,data.vx,self.Xvelcurve,self._widget.Xvelcheck)
+        self.VariableOnUpdate(self.Yvelplotvector,data.vy,self.Yvelcurve,self._widget.Yvelcheck)
+        self.VariableOnUpdate(self.Zvelplotvector,data.vz,self.Zvelcurve,self._widget.Zvelcheck)       
 
+        self.VariableOnUpdate(self.Xdvelplotvector,data.vxd,self.Xdvelcurve,self._widget.Xdvelcheck)
+        self.VariableOnUpdate(self.Ydvelplotvector,data.vyd,self.Ydvelcurve,self._widget.Ydvelcheck)
+        self.VariableOnUpdate(self.Zdvelplotvector,data.vzd,self.Zdvelcurve,self._widget.Zdvelcheck)
+    
+        self.VariableOnUpdate(self.Roll_plotvector,data.roll,self.Roll_curve,self._widget.Roll_check)
+        self.VariableOnUpdate(self.Pitch_plotvector,data.pitch,self.Pitch_curve,self._widget.Pitch_check)
+        self.VariableOnUpdate(self.Yaw_plotvector,data.yaw,self.Yaw_curve,self._widget.Yaw_check)
 
-        # ---------------------------------------------- #
-        # ---------------------------------------------- #
+        self.VariableOnUpdate(self.Rolld_plotvector,data.roll_d,self.Rolld_curve,self._widget.Rolld_check)
+        self.VariableOnUpdate(self.Pitchd_plotvector,data.pitch_d,self.Pitchd_curve,self._widget.Pitchd_check)
+        self.VariableOnUpdate(self.Yawd_plotvector,data.yaw_d,self.Yawd_curve,self._widget.Yawd_check)
+
+        self.VariableOnUpdate(self.Ch1plotvector,data.cmd_1,self.Ch1curve,self._widget.Ch1check)
+        self.VariableOnUpdate(self.Ch2plotvector,data.cmd_2,self.Ch2curve,self._widget.Ch2check)
+        self.VariableOnUpdate(self.Ch3plotvector,data.cmd_3,self.Ch3curve,self._widget.Ch3check)            
+        self.VariableOnUpdate(self.Ch4plotvector,data.cmd_4,self.Ch4curve,self._widget.Ch4check)  
+        
+        return
+
+    def callback(self,data):
+
+        self.combined_data.emit(data)
 
         # increase or reset counter plot data saving
         if self.counterPlot <= self.counterBoundPlot:
             self.counterPlot = self.counterPlot + 1
         else:
             self.counterPlot = 1
-
-
-        # ---------------------------------------------- #
-        # ---------------------------------------------- #
 
         # increase or reset counter plot data saving
         if self.counter <= self.counterBound:
@@ -510,76 +424,6 @@ class positionPlotPlugin(Plugin):
             self.counter = 1
 
 
-        # ---------------------------------------------- #
-        # ---------------------------------------------- #            
-
-        # emit data for x,y,z vectors to be updated
-        self.Xon.emit(data.x)
-        self.Yon.emit(data.y)
-        self.Zon.emit(data.z)
-        # emit data for desired x,y,z vectors to be updated
-        self.Xdon.emit(data.xd)
-        self.Ydon.emit(data.yd)
-        self.Zdon.emit(data.zd)
-        # emit data for velocities x,y,z vectors to be updated
-        self.Xvelon.emit(data.vx)
-        self.Yvelon.emit(data.vy)
-        self.Zvelon.emit(data.vz)
-        # emit data for desired velocities x,y,z vectors to be updated
-        self.Xdvelon.emit(data.vxd)
-        self.Ydvelon.emit(data.vyd)
-        self.Zdvelon.emit(data.vzd)
-        # emit data for euler angles vector to be updated
-        self.Roll_on.emit(data.roll)
-        self.Pitch_on.emit(data.pitch)
-        self.Yaw_on.emit(data.yaw)
-        # emit data for desired euler angles vector to be updated
-        self.Rolld_on.emit(data.roll_d)
-        self.Pitchd_on.emit(data.pitch_d)
-        self.Yawd_on.emit(data.yaw_d)        
-        # emit data for channels z vectors to be updated
-        self.Ch1on.emit(data.cmd_1)
-        self.Ch2on.emit(data.cmd_2)
-        self.Ch3on.emit(data.cmd_3)                           
-        self.Ch4on.emit(data.cmd_4)                           
-
-    # ------------------------------------------------------------------------------ #
-    # ------------------------------------------------------------------------------ #
-    # ------------------------------------------------------------------------------ #
-    # ------------------------------------------------------------------------------ #
-
-    def TimeUpdate(self,data):
-
-        if self.counter == 1:
-            self.timevector[:-1] = self.timevector[1:]
-            self.timevector[-1]  = data - self.time0
-
-
-    # def XonUpdate(self,data):
-
-        
-    #     # updata data vector for x component
-    #     if self.counter == 1:
-    #         self.Xplotvector[:-1] = self.Xplotvector[1:]
-    #         self.Xplotvector[-1]  = data
-
-    
-    #     # plot only after certain time interval, according to counter
-    #     if self.counterPlot == 1:
-
-    #         # plot if box for x component is ticked
-    #         if self._widget.Xcheck.isChecked():
-                
-    #             # uncomment when interested in cheching time between two data points
-    #             # print(self.timevector[-1] - self.timevector[-2])
-                
-    #             self.Xcurve.setData(self.timevector,self.Xplotvector)
-
-    #         else:
-    #             # clear plot 
-    #             self.Xcurve.setData([],[])
-        
-
     def VariableOnUpdate(self,VariablePlotVector,data,PlotHandle,WidgetChecked):
 
         # updata data vector for "x" component
@@ -587,106 +431,19 @@ class positionPlotPlugin(Plugin):
             VariablePlotVector[:-1] = VariablePlotVector[1:]
             VariablePlotVector[-1]  = data
 
-    
         # plot only after certain time interval, according to counter
         if self.counterPlot == 1:
 
             # plot if box for "x" component is ticked
             if WidgetChecked.isChecked():
-                
+           
                 # uncomment when interested in cheching time between two data points
-                # print(self.timevector[-1] - self.timevector[-2])
-                
+                # print(self.timevector[-1] - self.timevector[-2])                
                 PlotHandle.setData(self.timevector,VariablePlotVector)
 
             else:
                 # clear plot 
                 PlotHandle.setData([],[])
-
-
-    # ---------------------------------------------- #
-    # x, y, z         
-    def XonUpdate(self,data):
-        return self.VariableOnUpdate(self.Xplotvector,data,self.Xcurve,self._widget.Xcheck)
-
-    def YonUpdate(self,data):
-        return self.VariableOnUpdate(self.Yplotvector,data,self.Ycurve,self._widget.Ycheck)
-
-    def ZonUpdate(self,data):
-        return self.VariableOnUpdate(self.Zplotvector,data,self.Zcurve,self._widget.Zcheck)
-
-    # ---------------------------------------------- #
-    # desired x, y, z
-    def XdonUpdate(self,data):
-        return self.VariableOnUpdate(self.Xdplotvector,data,self.Xdcurve,self._widget.Xdcheck)
-
-    def YdonUpdate(self,data):
-        return self.VariableOnUpdate(self.Ydplotvector,data,self.Ydcurve,self._widget.Ydcheck)
-
-    def ZdonUpdate(self,data):
-        return self.VariableOnUpdate(self.Zdplotvector,data,self.Zdcurve,self._widget.Zdcheck)
-    
-    # ---------------------------------------------- #
-    # velocities x, y, z
-    def XvelonUpdate(self,data):
-        return self.VariableOnUpdate(self.Xvelplotvector,data,self.Xvelcurve,self._widget.Xvelcheck)
-
-    def YvelonUpdate(self,data):
-        return self.VariableOnUpdate(self.Yvelplotvector,data,self.Yvelcurve,self._widget.Yvelcheck)
-
-    def ZvelonUpdate(self,data):
-        return self.VariableOnUpdate(self.Zvelplotvector,data,self.Zvelcurve,self._widget.Zvelcheck)       
-
-    # ---------------------------------------------- #
-    # desired velocities x, y, z
-    def XdvelonUpdate(self,data):
-        return self.VariableOnUpdate(self.Xdvelplotvector,data,self.Xdvelcurve,self._widget.Xdvelcheck)
-
-    def YdvelonUpdate(self,data):
-        return self.VariableOnUpdate(self.Ydvelplotvector,data,self.Ydvelcurve,self._widget.Ydvelcheck)
-
-    def ZdvelonUpdate(self,data):
-        return self.VariableOnUpdate(self.Zdvelplotvector,data,self.Zdvelcurve,self._widget.Zdvelcheck)
-    
-    # ---------------------------------------------- #
-    # euler angles         
-    def Roll_onUpdate(self,data):
-        return self.VariableOnUpdate(self.Roll_plotvector,data,self.Roll_curve,self._widget.Roll_check)
-
-    def Pitch_onUpdate(self,data):
-        return self.VariableOnUpdate(self.Pitch_plotvector,data,self.Pitch_curve,self._widget.Pitch_check)
-
-    def Yaw_onUpdate(self,data):
-        return self.VariableOnUpdate(self.Yaw_plotvector,data,self.Yaw_curve,self._widget.Yaw_check)
-
-    # ---------------------------------------------- #
-    # desired euler angles
-    def Rolld_onUpdate(self,data):
-        return self.VariableOnUpdate(self.Rolld_plotvector,data,self.Rolld_curve,self._widget.Rolld_check)
-
-    def Pitchd_onUpdate(self,data):
-        return self.VariableOnUpdate(self.Pitchd_plotvector,data,self.Pitchd_curve,self._widget.Pitchd_check)
-
-    def Yawd_onUpdate(self,data):
-        return self.VariableOnUpdate(self.Yawd_plotvector,data,self.Yawd_curve,self._widget.Yawd_check)
-
-    # ---------------------------------------------- #
-    # channels 1, 2, 3 and 4
-    def Ch1onUpdate(self,data):
-        return self.VariableOnUpdate(self.Ch1plotvector,data,self.Ch1curve,self._widget.Ch1check)
-
-    def Ch2onUpdate(self,data):
-        return self.VariableOnUpdate(self.Ch2plotvector,data,self.Ch2curve,self._widget.Ch2check)
-
-    def Ch3onUpdate(self,data):
-        return self.VariableOnUpdate(self.Ch3plotvector,data,self.Ch3curve,self._widget.Ch3check)            
-        
-    def Ch4onUpdate(self,data):
-        return self.VariableOnUpdate(self.Ch4plotvector,data,self.Ch4curve,self._widget.Ch4check)          
-    # ------------------------------------------------------------------------------ #
-    # ------------------------------------------------------------------------------ #
-    # ------------------------------------------------------------------------------ #
-    # ------------------------------------------------------------------------------ #  
 
 
     def SetSubscription(self):
