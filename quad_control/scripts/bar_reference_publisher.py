0a85738bc9b28ddb6b51b1ecdfef45f37c74ddf0
massimilianop <massimiliano@gmx.com>
2017-01-23 18:51:31 +0100

    Reference publisher with a method for a slow lift-off (10 cm every 10 sec).

--- quad_control/scripts/bar_reference_publisher.py
+++ quad_control/scripts/bar_reference_publisher.py
@@ -25,16 +25,31 @@ class BarReferencePublisher():
         while not rospy.is_shutdown():
 
             message_instance = nav_msgs.msg.Path()
-            p1 = geometry_msgs.msg.PoseStamped()
-            p2 = geometry_msgs.msg.PoseStamped()
-            # p2.pose.position.x = 1
-            p1.pose.position.z = 0.5
-            p2 = p1
-            message_instance.poses = [p1,p2]
+            # p1 = geometry_msgs.msg.PoseStamped()
+            # p2 = geometry_msgs.msg.PoseStamped()
+            # # p2.pose.position.x = 1
+            # p1.pose.position.z = 0.5
+            # p2 = p1
+            # message_instance.poses = [p1,p2]
+            message_instance.poses = self.liftOff()
             publish_reference.publish(message_instance)
 
             # go to sleep
             rate.sleep()
+
+
+    def liftOff(self,n=5,step=0.1,timeStep=10):
+        path = nav_msgs.msg.Path()
+
+        for i in range(1,n+1):
+            path.poses.append(geometry_msgs.msg.PoseStamped())
+            path.poses[i-1].pose.position.z = step * i
+            path.poses[i-1].header.stamp.secs    = timeStep * i
+
+        return path.poses
+
+
+
 
 if __name__ == '__main__':
     bar_reference_publisher = BarReferencePublisher()
